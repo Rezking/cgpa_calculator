e4f0f73ef76be733b7007732e3f68f39381e050e
Rezking <onagoreoluwa92@gmail.com>
2022-08-10 09:08:50 +0100

    added test

---
+++ src/TrytoTest.java
@@ -0,0 +1,8 @@
+public class TrytoTest {
+    public static void main(String[] args){
+        int test1 = Main.calculatePoint("A",3);
+        if (test1 == 15)
+            System.out.println("Test passed");
+        else System.out.println("Test Failed");
+    }
+}
